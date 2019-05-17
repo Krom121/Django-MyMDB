a3dfc9342b2eacd7b76c4bdc5dcd15e737a51764
krom121 <stephenacgregor9@gmail.com>
2019-05-17 11:33:46 +0100

    movie model+objects in database python shell

--- config/settings.py
+++ config/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mymdb',
-        'USER': 'mymdb',
-        'PASSWORD': 'development',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
