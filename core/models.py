8c84f00edaf431e9ef3169427498121b60cb7547
krom121 <stephenacgregor9@gmail.com>
2019-05-18 10:40:23 +0100

    Added person model+views

--- core/models.py
+++ core/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+
+class MovieManager(models.Manager):
+    
+    def all_with_related_persons(self):
+        qs = self.get_queryset()
+        qs = qs.select_related(
+            'director')
+        qs = qs.prefetch_related(
+            'writers', 'actors')
+        return qs
+
+
+
 class Movie(models.Model):
     NOT_RATED = 0
     RATED_G = 1
@@ -24,6 +37,8 @@ class Movie(models.Model):
         blank=True)
     writers = models.ManyToManyField(to='Person', related_name='writing_credits', blank=True)
     actors = models.ManyToManyField(to='Person', through='Role', related_name='acting_credits', blank=True)
+    objects = MovieManager()
+    
     class Meta:
         ordering = ('-year', 'title')
 
@@ -31,11 +46,21 @@ class Movie(models.Model):
         return '{} ({})'.format(self.title, self.year)
 
 
+class PersonManager(models.Manager):
+    def all_with_prefetch_movies(self):
+        qs = self.get_queryset()
+        return qs.prefetch_related('directed', 'writing_credits', 'role_set__movie')
+
+
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=140)
     last_name = models.CharField(max_length=140)
     born = models.DateField()
     died = models.DateField(null=True, blank=True)
+
+    objects = PersonManager()
 
     class Meta:
         ordering = (
--- core/views.py
+++ core/views.py
@@ -1,10 +1,16 @@
 from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator
-from core.models import Movie
+from core.models import Movie , Person
 
 class MovieDetail(DetailView):
     model = Movie
+    queryset = (Movie.objects.all_with_related_persons())
 
 class MovieList(ListView):
     model = Movie
     paginator = Paginator(Movie,3)
+
+
+class PersonDetail(DetailView):
+    model = Person
+    queryset = Person.objects.all_with_prefetch_movies()
