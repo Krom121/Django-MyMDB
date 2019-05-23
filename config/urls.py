52ddba04ff8b7a6a5ad641f9d1a74f736d8d9e56
krom121 <stephenacgregor9@gmail.com>
2019-05-23 10:03:39 +0100

    register+urls+views

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from django.contrib import admin
 
 import core.urls
+import user.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(core.urls, namespace='core')),
+    path('user/', include(user.urls, namespace='user')),
 ]
